142015c1820181548a057db5f1bf2802e39d33e8
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-05-30 23:41:52 -0400

    I did some work. None of it works. But I tried.

--- README.md
+++ README.md
@@ -4,9 +4,13 @@
 ## Pseudocode
 
 When the user clicks a button, prompts appear in this order:
-- Choose length (between & including 28 and 128 chars)
+- Choose length (between & including 8 and 128 chars)
 - Use square boxes to allow them to choose lowercase, uppercase, numeric, and/or special characters - at least one type must be selected
 - Keep next button from being "clickable" til they select one choice
+
+make sure there's a minimum of 1 from each array
+
+insert into a new array all the complex rules that you need and randomize your final array
 
 Then generate a password based on the criteria.
 
--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_", "'"];
+
 // Write password to the #password input
-function writePassword() {
+
+}
+
+// Make an array with the user preferences
+function generateArray1 ()
+
+//Make a random password from the array
+function randomPassword() {
+  var length = num1,
+      charset = array1,
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+// Password Generator
+function generatePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,4 +34,16 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword){
+    let a1 = prompt("How many characters do you want to enter? Must be between 8 and 128", "0");
+    var num1 = parseInt(a1);
+  
+    if (num1 > 128 &&& < 8){
+      var yesUpperCase = confirm("Do you want to use uppercase lettesr?")
+      if (yesUpperCase = true){
+        return yesUpperCase)
+}
+    }
+
+  }  
+  }
